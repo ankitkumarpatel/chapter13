252a916630dd6832194fa8f9e3ecac01f28e6990
ankitkumarpatel <ankitkumar.patel@dal.ca>
2019-01-31 22:30:40 -0800

    splitted aeach line into words andremoved punctuations and whitespaces and then printed each word

--- 13.1.py
+++ 13.1.py
@@ -5,6 +5,9 @@ import string
 fin= open("bookzzz.txt")
 
 for line in fin:
-    line= line.strip(string.punctuation+string.whitespace+" ")
 
-    print(line)
+   for word in line.split():
+       # line= line.strip(string.punctuation+string.whitespace+" ")
+       # word= line.strip(string.punctuation+sring.whitespace) 
+       word= word.strip(string.punctuation+string.whitespace)
+       print(word)
