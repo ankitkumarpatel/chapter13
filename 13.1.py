76fbb2d3f5e8063db4299838de1288b9a0c86643
ankitkumarpatel <ankitkumar.patel@dal.ca>
2019-01-31 22:50:19 -0800

    converted each word in to lowercase then printed each word

--- 13.1.py
+++ 13.1.py
@@ -2,12 +2,12 @@
 
 import string
 
+
+
 fin= open("bookzzz.txt")
 
 for line in fin:
-
    for word in line.split():
-       # line= line.strip(string.punctuation+string.whitespace+" ")
-       # word= line.strip(string.punctuation+sring.whitespace) 
-       word= word.strip(string.punctuation+string.whitespace)
+       word= word.strip(string.punctuation+string.whitespace).lower()
        print(word)
+
